b6ff9bbe77b7489067c1ebaff80eb55b33ce4253
Philip <philwsun@gmail.com>
2024-08-19 21:15:53 -0400

    My changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,17 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- My stuff:
+map("n", "<leader>h", "<C-w>h")
+map("n", "<leader>j", "<C-w>j")
+map("n", "<leader>k", "<C-w>k")
+map("n", "<leader>l", "<C-w>l")
+
+map("n", "<leader>1", "1gt")
+map("n", "<leader>2", "2gt")
+map("n", "<leader>3", "3gt")
+map("n", "<leader>4", "4gt")
+
+map("n", "j", "gj")
+map("n", "k", "gk")
